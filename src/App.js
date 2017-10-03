d77abd14eb843159c4aa9d4bb395945db13f751a
yonggoonoh <nohyg90@gmail.com>
2017-10-04 00:21:13 +0900

    complete basic movie app

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie'
+
+class App extends Component {
+	state = {};
+
+	componentDidMount() {
+		this._getMovies();
+	}
+
+	// async function
+	_getMovies = async () => {
+		// _callAPI가 완료되기 전 까지 대기한다.
+		// async function에서만 가능함
+		const movies = await this._callAPI();
+
+		// await가 완료된 후에 실행됨. 그것의 결과가 성공/실패든
+		this.setState({
+			// equivalent to `movies: movies`
+			movies
+		})
+	};
+
+	_callAPI = () => {
+		// fetch는 promise를 return하는데, 이는 then과 catch로 다룰 수 있다. 
+		return fetch("https://yts.ag/api/v2/list_movies.json?sort_by=download_count")
+		.then(response => response.json())
+		.then(json => json.data.movies)
+		.catch(err => console.log(err));
+	};
+
+	_renderMovies = () => {
+		const movies = this.state.movies.map((movie) => {
+			return (<Movie
+				title={movie.title_english} 
+				poster={movie.medium_cover_image}
+				genres={movie.genres}
+				synopsis={movie.synopsis}
+				key={movie.id}
+			/>);
+		});
+
+		return movies;
+	};
+
+	render() {
+        const { movies } = this.state;
+		return (
+			<div className={movies ? "App" : "App--loading"}>
+				{this.state.movies ? this._renderMovies() : "Loading..."}
+			</div>
+		);
+	}
+}
+
+export default App
---
+++ src/Movie.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}) {
+	return (
+		<div className="Movie">
+			<div className="Movie__Column">
+				<MoviePoster poster={poster} alt={title} />
+			</div>
+			<div className="Movie__Column">
+				<h3>{title}</h3>
+				<div className="Movie__Genres">
+					{genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+				</div>
+				<p className="Movie__Synopsis">
+					{synopsis}
+				</p>
+			</div>
+		</div>
+	);
+}
+
+function MoviePoster({poster, alt}) {
+	return (
+		<img src={poster} title={alt} alt={alt} className="Movie__Poster" />
+	);
+}
+
+function MovieGenre({genre}) {
+	return (
+        <span className="Movie_Genre">{genre} </span>
+    );
+}
+
+Movie.propTypes = {
+	title: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired,
+	genres: PropTypes.array.isRequired,
+	synopsis: PropTypes.string.isRequired,
+};
+
+MoviePoster.propTypes = {
+	poster: PropTypes.string.isRequired,
+	alt: PropTypes.string.isRequired,
+};
+
+MovieGenre.propTypes = {
+	genre: PropTypes.string.isRequired,
+};
+
+export default Movie
