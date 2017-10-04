2094a020b94a8cca7965f712bab0b3315458af7e
yonggoonoh <nohyg90@gmail.com>
2017-10-04 22:54:27 +0900

    header state 변경 가능

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import '../../css/Header.css';
 
 class Header extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			sortValue: "download",
+			limitValue: "20",
+		};
+
+		this._handleChange = this._handleChange.bind(this);
+	}
+
+	_handleChange(e) {
+		let nextState = {};
+		nextState[e.target.name] = e.target.value;
+		this.setState(nextState);
+	}
+
 	render() {
 		return (
 			<div className="header">
 				<ul>
 					<li>
 						<label htmlFor="sort_by">sort by </label>
-						<select id="sort_by" value={"like"} onChange={()=>{}}>
+						<select id="sort_by" name="sortValue" value={this.state.sortValue} onChange={this._handleChange}>
 							<option value="download">download count</option>
 							<option value="like">like count</option>
 							<option value="rating">rating</option>
@@ -17,7 +34,7 @@ class Header extends Component {
 					</li>
 					<li>
 						<label htmlFor="limit">limit </label>
-						<input type="number" id="limit" min="1" max="50" value="20" onChange={()=>{}}/>
+						<input type="number" id="limit" name="limitValue" min="1" max="50" value={this.state.limitValue} onChange={this._handleChange} />
 					</li>
 					<li>
 						<button>search!</button>
