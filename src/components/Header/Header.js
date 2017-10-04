50e4e865e9f015e9df5372f316f56fb48fb87341
yonggoonoh <nohyg90@gmail.com>
2017-10-04 22:22:37 +0900

    Header 구조 잡기

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,28 @@ import '../../css/Header.css';
 class Header extends Component {
 	render() {
 		return (
-	        <div class="header">
-				<h1>Header</h1>
+			<div className="header">
+				<ul>
+					<li>
+						<label htmlFor="sort_by">sort by </label>
+						<select id="sort_by" value={"like"} onChange={()=>{}}>
+							<option value="download">download count</option>
+							<option value="like">like count</option>
+							<option value="rating">rating</option>
+							<option value="date_added">date added</option>
+						</select>
+					</li>
+					<li>
+						<label htmlFor="limit">limit </label>
+						<input type="number" id="limit" min="1" max="50" value="20" onChange={()=>{}}/>
+					</li>
+					<li>
+						<button>search!</button>
+					</li>
+				</ul>
 			</div>
 		)
 	}
-
 }
 
 export default Header
